52b588fc501e912d4598b4450249300f0dc138e2
Mark.Etzel@gmx.email <Mark.Etzel@gmx.de>
2020-10-23 16:53:27 +0200

    imported travel destinations

--- src/app/travels.ts
+++ src/app/travels.ts
@@ -1,14 +1,14 @@
 export const travels = [
     {
         name: "Acatenango",
-        image: "./assets/acatenango",
-        description: "",
+        image: "./assets/acatenango.jpg",
+        description: "The Acatenango is a stratovolcano in Guatemala, close to the city of Antigua. It is joined with Volcán de Fuego and collectively the volcano complex is known as La Horqueta. Fuego is famous for being almost constantly active at a low level. Small gas and ash eruptions occur every 15 to 20 minutes and can be observed well from the Acatenango",
         startDate: "09.03.2021",
         duration: "2 days",
         price: 50
     }, {
         name: "Guatapé",
-        image: "./assets/guatape",
+        image: "./assets/guatape.jpg",
         description: "Guatapé is a town and municipality in the Department of Antioquia, Colombia. The town is a major tourist attraction, especially for travelers from Medellín, for both El Peñón de Guatapé, a large rock that visitors can climb, and the famous zócalos(baseboards) that adorn several of the buildings.",
         startDate: "30.03.2021",
         duration: "1 day",
@@ -22,7 +22,7 @@ export const travels = [
         price: 120
     }, {
         name: "Cañón del Colca",
-        image: "./assets/canondelcolca",
+        image: "./assets/canondelcolca.jpg",
         description: "The Colca Canyon is a canyon of the Colca River in southern Peru, located about 160 kilometres northwest of Arequipa. With a depth of 3,270 metres, it is one of the deepest in the world. The Colca Valley is a colorful Andean valley with pre-Inca roots, and towns founded in Spanish colonial times, still inhabited by people of the Collagua and the Cabana cultures.",
         startDate: "13.04.2021",
         duration: "2 days",
